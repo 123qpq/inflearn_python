974d9daf401662b9ae3d076e6cad8cf92d0d2b83
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-14 14:25:02 +0900

    Update k번째 약수.py
    better code

--- k번째 약수.py
+++ k번째 약수.py
@@ -1,12 +1,12 @@
 import sys
 
 n, k = map(int, sys.stdin.readline().split())
-lst = []
+cnt = 0
 for i in range(1, n+1):
     if n % i == 0:
-        lst.append(i)
-
-if len(lst) < k:
-    print(-1)
+        cnt += 1
+    if cnt == k:
+        print(i)
+        break
 else:
-    print(lst[k-1])
+    print(-1)
