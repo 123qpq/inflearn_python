d25c0cdd548a9f0761ca29d3dfc632b2ee4f3b95
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-15 20:44:42 +0900

    Update 회문 문자열 검사.py
    better answer

--- 회문 문자열 검사.py
+++ 회문 문자열 검사.py
@@ -4,8 +4,7 @@ n = int(sys.stdin.readline())
 
 for i in range(n):
     word = input().lower()
-    wlen = len(word) // 2
-    if word[:wlen] == word[:len(word)-wlen-1:-1]:
+    if word == word[::-1]:
         print(f"#{i+1} YES")
     else:
         print(f"#{i+1} NO")
