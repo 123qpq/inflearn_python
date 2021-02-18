139a076cd371c19795be076333aff1c6b52aecca
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-18 11:18:45 +0900

    Update 침몰하는 타이타닉.py
    more faster with deque

--- binary search and greedy/침몰하는 타이타닉.py
+++ binary search and greedy/침몰하는 타이타닉.py
@@ -1,11 +1,17 @@
+from collections import deque
+
 n, maxweights = map(int, input().split())
 weights = list(map(int, input().split()))
 cnt = 0
 weights.sort()
+weights = deque(weights)
 while weights:
+    if len(weights) == 1:
+        cnt += 1
+        break
     if weights[0] + weights[-1] <= maxweights:
         weights.pop()
-        weights.pop(0)
+        weights.popleft()
     else:
         weights.pop()
     cnt += 1
