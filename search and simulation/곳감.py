234d0aff9b79ab600403c004427cc96f23e932c6
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-18 10:27:45 +0900

    Update 곳감.py
    another answer. more fester than my answer
    this code : 3.6s
    my code : 9.9s

--- search and simulation/곳감.py
+++ search and simulation/곳감.py
@@ -8,10 +8,11 @@ for _ in range(m):
     row, direc, rot = map(int, input().split())
     rot %= n
     if direc: #오른쪽
-        table[row-1] = table[row-1][n - rot:] + table[row-1][:n - rot]
+        for _ in range(rot):
+            table[row-1].insert(0, table[row-1].pop())
     else: #왼쪽
-        table[row-1] = table[row-1][rot:] + table[row-1][:rot]
-    #print(table)
+        for _ in range(rot):
+            table[row-1].append(table[row-1].pop(0))
 
 for i in range(n//2):
     res += sum(table[i][i:n-i])
