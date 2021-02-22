d305d7d585916b179963525aa0c44612371eb29d
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-22 12:19:57 +0900

    Create 바둑이 승차.py

---
+++ exhaustive search/바둑이 승차.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def dfs(l, sum, tsum):
+    global res
+    if sum + (total-tsum) < res:
+        return
+    if sum > c:
+        return
+    if l == n:
+        if sum > res:
+            res = sum
+    else:
+            dfs(l+1, sum+a[l], tsum+a[l])
+            dfs(l+1, sum, tsum+a[l])
+
+if __name__ == "__main__":
+    c, n = map(int, input().split())
+    a = [int(input()) for _ in range(n)]
+    total = sum(a)
+    res = 0
+    dfs(0, 0, 0)
+    print(res)
