bb5d09f67f50b25c8305c64c681bdd5705ebe06a
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-22 12:33:03 +0900

    Create 순열 구하기.py

---
+++ exhaustive search/순열 구하기.py
@@ -0,0 +1,23 @@
+def dfs(l):
+    global cnt
+    if l == m:
+        for j in range(l):
+            print(res[j], end = " ")
+        print()
+        cnt += 1
+    else:
+        for i in range(1, n+1):
+            if check[i] == 0:
+                check[i] =1
+                res[l] = i
+                dfs(l+1)
+                check[i]=0
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    res = [0] * n
+    check = [0] * (n+1)
+    cnt = 0
+    dfs(0)
+    print(cnt)
