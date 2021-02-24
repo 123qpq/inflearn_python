813d6730b3aabdbc59378b151ac4ef9cb0e22ca7
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-24 23:31:48 +0900

    Update 송아지 찾기.py
    my answer

--- apply dfs and bfs/송아지 찾기.py
+++ apply dfs and bfs/송아지 찾기.py
@@ -1,19 +1,15 @@
-from collections import deque
-MAX = 10000
-ch = [0] * (MAX + 1)
-dis = [0] * (MAX + 1)
-n, m = map(int, input().split())
-ch[n] = 1
-dis[n] = 0
-dq = deque()
-dq.append(n)
-while dq:
-    now = dq.popleft()
-    if now == m:
-        break
-    for next in (now-1, now+1, now+5):
-        if 0 < next <= MAX and ch[next] == 0:
-                dq.append(next)
-                ch[next] = 1
-                dis[next] = dis[now] + 1
-print(dis[m])
+start, end = map(int, input().split())
+length = end - start
+if length < 0:
+    print(length)
+else:
+    res = 0
+    res += length // 5
+    rest = length % 5
+    if rest == 0:
+        pass
+    elif rest < 4:
+        res += rest
+    elif rest == 4:
+        res += 2
+    print(res)
