3dfcbcf746cede0e4043ea47f5ff64eb78da5bf3
123qpq <45002168+123qpq@users.noreply.github.com>
2021-02-25 11:53:54 +0900

    Create 미로탐색.py

---
+++ apply dfs and bfs/미로탐색.py
@@ -0,0 +1,21 @@
+def dfs(x, y):
+    global cnt
+    if x == 6 and y == 6:
+        cnt += 1
+    else:
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0 <= xx < 7 and 0 <= yy < 7 and table[xx][yy] == 0:
+                table[xx][yy] = 1
+                dfs(xx, yy)
+                table[xx][yy] = 0
+
+if __name__ == "__main__":
+    table = [list(map(int, input().split())) for _ in range(7)]
+    cnt = 0
+    dx = [1, 0, -1, 0]
+    dy = [0, -1, 0, 1]
+    table[0][0] = 1
+    dfs(0, 0)
+    print(cnt)
